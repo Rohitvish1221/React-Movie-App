0386b107f4f46a15053b2ddbed9c1d321cfe6557
Rohit Vishwakarma <vishro1221@gmail.com>
2025-08-02 03:44:22 +0530

    Edited

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,25 +5,24 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./Components/NavBar";
 import MovieSearch from "./Components/MovieSearch";
 import SearchResults from "./Pages/SearchResults";
-import MovieDetails from "./Pages/MovieDetails"; 
+import MovieDetails from "./Pages/MovieDetails";
 
 function App() {
   return (
-    <div>
-      <NavBar />
-      <MovieSearch/>
-      <main className="main-content">
-        <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/search" element={<SearchResults />} />
-          <Route path="/favorites" element={<Favorites />} />
-          <Route path="/movie/:id" element={<MovieDetails />} />
-        </Routes>
-        </BrowserRouter>
-
-      </main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <NavBar />
+        <MovieSearch />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/search" element={<SearchResults />} />
+            <Route path="/favorites" element={<Favorites />} />
+            <Route path="/movie/:id" element={<MovieDetails />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
